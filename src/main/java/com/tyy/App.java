4b0e2d0553c2224198dae853a662062eb26adfd1
FightingInvincible <1193385564@qq.com>
2020-07-13 13:23:52 +0800

    完成Maven搭建

---
+++ src/main/java/com/tyy/App.java
@@ -0,0 +1,13 @@
+package com.tyy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
